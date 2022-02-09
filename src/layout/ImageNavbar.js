780f33cb24ce0ebc8ac466a8374f204c6fd8d109
Tom Magin <magintc@mac.com>
2022-02-09 23:17:10 +1100

    screwing around on css

--- src/layout/ImageNavbar.js
+++ src/layout/ImageNavbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import i from "../css/imagenavbar.module.css";
+
 const ImageNavbar = ({ chars }) => {
   return (
     <div className={i.navbar}>
@@ -11,19 +12,6 @@ const ImageNavbar = ({ chars }) => {
           alt="no_rickmorty_pic"
         />
       ))}
-      <div className={i.middle}>
-        <div className={i.text}>
-          {chars.map((c) => (
-            <p>
-              Name:{c.name}
-              <br />
-              Species:{c.species}
-              <br />
-              No. Eps:{c.episode.length}
-            </p>
-          ))}
-        </div>
-      </div>
     </div>
   );
 };
